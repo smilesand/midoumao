2f01ed5f6d82bc5e6b48bd03e3fc818c1563945a
midoumao <847666418@qq.com>
2025-03-13 13:52:44 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-        <meta name="google-adsense-account" content="ca-pub-8772395830306978">
     </header>
   );
 }
